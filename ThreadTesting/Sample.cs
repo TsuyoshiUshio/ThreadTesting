161948285b82c064dec61c73dd2b91217cba69f4
Tsuyoshi Ushio <ushio@simplearchitect.com>
2022-03-17 13:47:43 -0700

    Add project files.

---
+++ ThreadTesting/Sample.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadTesting
+{
+    public class Sample
+    {
+        public void Execute()
+        {
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (source, args) =>
+            {
+                Console.Error.WriteLine("Cancelling execution ...");
+                args.Cancel = true;
+                cts.Cancel();
+            };
+
+            Task.Run(() => HelloLoopAsync(cts.Token));
+            Task.Run(() => HelloLoopAsync(cts.Token));
+            Console.ReadLine();
+        }
+
+        private async Task HelloLoopAsync(CancellationToken cancellationToken)
+        {
+            int count = 0;
+            while(!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(1000 * 3);
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: count : {count}");
+                count++;
+            }
+            Console.WriteLine($"Cancelled {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
